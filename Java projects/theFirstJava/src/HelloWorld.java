4b418ec1f73d7af07aba14c56296bfeb85271a07
pourquoi21 <miyaku219@naver.com>
2023-04-28 14:43:42 +0900

    Java projects

---
+++ Java projects/theFirstJava/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args){
+    Singleton a = Singleton.getInstance();
+    Singleton b = Singleton.getInstance();
+    System.out.println(a.hashCode());
+    System.out.println(b.hashCode());
+    if(a == b){
+        System.out.println(true);
+    }
+}
+}
