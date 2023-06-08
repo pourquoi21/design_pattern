1e3cffdb7d2c9dee2fca84b5ec1794adf50a7eb5
pourquoi21 <miyaku219@naver.com>
2023-06-09 07:48:52 +0900

    learning about basic concepts..

---
+++ Java projects/theFirstJava/src/MyProfile.java
@@ -0,0 +1,7 @@
+public class MyProfile {
+    public static void main(String[] args){
+        System.out.print("Jay");
+        System.out.print("jay@jayjay.com");
+        System.out.print("female")
+    }
+}
