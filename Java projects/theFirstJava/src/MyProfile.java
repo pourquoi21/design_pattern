cb6df165519113e550ec917385b03e38fa249f98
pourquoi21 <miyaku219@naver.com>
2023-06-09 08:13:34 +0900

    while

--- Java projects/theFirstJava/src/MyProfile.java
+++ Java projects/theFirstJava/src/MyProfile.java
@@ -4,15 +4,20 @@ public class MyProfile {
         System.out.println("jay@jayjay.com");
         System.out.println("female");
         System.out.println("");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.println("**********");
-        System.out.print("**********");
+        // System.out.println("**********");
+        // System.out.println("**********");
+        // System.out.println("**********");
+        // System.out.println("**********");
+        // System.out.println("**********");
+        // System.out.println("**********");
+        // System.out.println("**********");
+        // System.out.println("**********");
+        // System.out.println("**********");
+        // System.out.print("**********");
+        int i = 1;
+        while(i <= 10) {
+            System.out.println("**********");
+            i += 1;
+        }
     }
 }
