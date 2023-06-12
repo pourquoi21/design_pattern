0e09b7e5c721014b3585a424dc77079868887396
pourquoi21 <miyaku219@naver.com>
2023-06-13 08:49:50 +0900

    editing methods

--- FirstJavaProjectWithin28minutes/src/com/in28minutes/firstjavaproject/MultiplicationTable.java
+++ FirstJavaProjectWithin28minutes/src/com/in28minutes/firstjavaproject/MultiplicationTable.java
@@ -2,8 +2,21 @@ package com.in28minutes.firstjavaproject;
 
 public class MultiplicationTable {
 	void print() {
-		for(int i = 1; i<=10; i++) {
-			System.out.printf("%d * %d = %d", 5, i, 5*i).println();
+		for(int i = 1; i <= 10; i++) {
+			System.out.printf("%d * %d = %d", 5, i, 5 * i).println();
+		}
+	}
+	
+	void print(int table) {
+//		for(int i = 1; i <= 10; i++) {
+//			System.out.printf("%d * %d = %d", table, i, table * i).println();
+//		}
+		print(6, 1, 10);
+	}
+	
+	void print(int table, int from, int to) {
+		for(int i = from; i <= to; i++) {
+			System.out.printf("%d * %d = %d", table, i, table * i).println();
 		}
 	}
 }
--- FirstJavaProjectWithin28minutes/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
+++ FirstJavaProjectWithin28minutes/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -5,7 +5,9 @@ public class MultiplicationTableRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MultiplicationTable table = new MultiplicationTable();
-		table.print();
+//		table.print();
+		table.print(5);
+		table.print(7, 2, 6);
 	}
 
 }
