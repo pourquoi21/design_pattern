487dbb805662681ab47f1638e9a8de9978444ec3
pourquoi21 <miyaku219@naver.com>
2023-06-15 08:43:10 +0900

    new project

---
+++ introduction_to_object_oriented_programming/src/com/in28minutes/oops/MotorBikeRunner.java
@@ -0,0 +1,14 @@
+package com.in28minutes.oops;
+
+public class MotorBikeRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MotorBike ducati = new MotorBike();
+		MotorBike honda = new MotorBike();
+
+		ducati.start();
+		honda.start();
+	}
+
+}
---
+++ introduction_to_object_oriented_programming/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Jara
+ *
+ */
+module introduction_to_object_oriented_programming {
+}
