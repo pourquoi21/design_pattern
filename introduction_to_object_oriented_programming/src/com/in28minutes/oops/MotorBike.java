aea6ef07ac184e25d41b9daa5ac3a4491f259e69
pourquoi21 <miyaku219@naver.com>
2023-06-17 23:50:09 +0900

    encapsulation

--- introduction_to_object_oriented_programming/src/com/in28minutes/oops/MotorBike.java
+++ introduction_to_object_oriented_programming/src/com/in28minutes/oops/MotorBike.java
@@ -1,7 +1,11 @@
 package com.in28minutes.oops;
 
 public class MotorBike {
-	int speed;
+	private int speed; // member variable
+
+	void setSpeed(int speed) { // local variable
+		System.out.println(this.speed);
+	}
 
 	void start() {
 		System.out.println("vrooooom!!");
