80400c087b06b5ed55289838944d0c56485528d6
pourquoi21 <miyaku219@naver.com>
2023-06-26 23:34:43 +0900

    changed methods contents

--- introduction_to_object_oriented_programming/src/com/in28minutes/oops/Binumber.java
+++ introduction_to_object_oriented_programming/src/com/in28minutes/oops/Binumber.java
@@ -1,38 +1,46 @@
 package com.in28minutes.oops;
 
 public class Binumber {
-	private int firstNumber;
-	private int secondNumber;
+	private int number1;
+	private int number2;
 
 	Binumber() {
 		this(2, 3);
 	}
 
 	Binumber(int firstNumber, int secondNumber) {
-		this.firstNumber = firstNumber;
-		this.secondNumber = secondNumber;
+		this.number1 = firstNumber;
+		this.number2 = secondNumber;
 
 	}
 
 	public int add() {
-		return (firstNumber + secondNumber);
+		return (number1 + number2);
 	}
 
 	public int multiply() {
-		return (firstNumber * secondNumber);
+		return (number1 * number2);
 	}
 
 	public void twice() {
-		this.firstNumber *= 2;
-		this.secondNumber *= 2;
+		this.number1 *= 2;
+		this.number2 *= 2;
 	}
 
 	public int getNumber1() {
-		return firstNumber;
+		return number1;
 	}
 
 	public int getNumber2() {
-		return secondNumber;
+		return number2;
+	}
+
+	public void setNumber1(int number1) {
+		this.number1 = number1;
+	}
+
+	public void setNumber2(int number2) {
+		this.number2 = number2;
 	}
 
 }
