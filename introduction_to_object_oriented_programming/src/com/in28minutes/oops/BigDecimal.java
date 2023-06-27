bc5d36ef6dac07d8b6bde7211037f80650176d49
pourquoi21 <miyaku219@naver.com>
2023-06-27 23:16:24 +0900

    bigdecimal

---
+++ introduction_to_object_oriented_programming/src/com/in28minutes/oops/BigDecimal.java
@@ -0,0 +1,13 @@
+package com.in28minutes.oops;
+
+public class BigDecimal {
+	public BigDecimal(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		BigDecimal number1 = new BigDecimal("34.56789");
+
+	}
+
+}
