fbb6ee75a29776a9d5d5a4856edfb37db4c25f6e
pourquoi21 <miyaku219@naver.com>
2023-06-29 22:29:58 +0900

    used BigDecimal

--- introduction_to_object_oriented_programming/src/com/in28minutes/oops/Calculator.java
+++ introduction_to_object_oriented_programming/src/com/in28minutes/oops/Calculator.java
@@ -2,20 +2,21 @@ package com.in28minutes.oops;
 
 import java.math.BigDecimal;
 
-public class SimpleInterestCalculator {
+public class Calculator {
 
 	BigDecimal principal;
 	BigDecimal interest;
 
-	SimpleInterestCalculator(String principal, String interest) {
+	public Calculator(String principal, String interest) {
 		this.principal = new BigDecimal(principal);
 		this.interest = new BigDecimal(interest).divide(new BigDecimal(100));
 	}
 
 	public BigDecimal calculateTotalValue(int noOfYears) {
 
-		// total value = principal + principal * interest * noOfYears;
-		BigDecimal totalValue = principal.add(principal.multiply(interest).multiply(new BigDecimal(noOfYears)));
+		BigDecimal bigDecimalNoOfYears = new BigDecimal(noOfYears);
+
+		BigDecimal totalValue = principal.add(principal.multiply(interest).multiply(bigDecimalNoOfYears));
 		return totalValue;
 	}
 
