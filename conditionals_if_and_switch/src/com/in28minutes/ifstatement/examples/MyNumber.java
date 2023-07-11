825c2447d1c1f0033bcdb5b862f73155e5f563af
pourquoi21 <miyaku219@naver.com>
2023-07-11 23:22:18 +0900

    making a new method ahhh

---
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/MyNumber.java
@@ -0,0 +1,20 @@
+package com.in28minutes.ifstatement.examples;
+
+public class MyNumber {
+    int number;
+
+    public MyNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isPrime() {
+
+        for (int i = 0; i <= number; i++) {
+
+        }
+
+        return false;
+
+    }
+
+}
