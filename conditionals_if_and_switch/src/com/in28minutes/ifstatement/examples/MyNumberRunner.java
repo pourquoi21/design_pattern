f6404c4a098fe8fb3e54d905f2fbcf57658897d2
pourquoi21 <miyaku219@naver.com>
2023-07-11 23:22:46 +0900

    commit

--- conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/MyNumberRunner.java
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/MyNumberRunner.java
@@ -3,7 +3,7 @@ package com.in28minutes.ifstatement.examples;
 public class MyNumberRunner {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        MyNumber number = new MyNumber(9);
 
     }
 
