de95ad569b496f624abd9de58f68f1cfc065b986
pourquoi21 <miyaku219@naver.com>
2023-07-13 22:45:57 +0900

    somesimplemethods

--- conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/MyNumber.java
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/MyNumber.java
@@ -15,14 +15,46 @@ public class MyNumber {
             }
 
             if (number % i == 0) {
-                if (i == 1)
+                if (i == 1) {
                     continue;
-                if (i == number)
+                }
+
+                if (i == number) {
                     return true;
+                }
                 return false;
             }
         }
         return false;
+    }
+
+    public int sumUptoN() {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+            if (i == number) {
+                return sum;
+            }
+        }
+        return sum;
+    }
+
+    public int sumOfDivisors() {
+        int divSum = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                divSum += i;
+            } else {
+                continue;
+            }
+        }
+        return divSum;
+    }
+
+    public void printANumberTriangle() {
+        for (int i = 1; i <= number; i++) {
+
+        }
     }
 
 }
--- conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/MyNumberRunner.java
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/MyNumberRunner.java
@@ -3,8 +3,12 @@ package com.in28minutes.ifstatement.examples;
 public class MyNumberRunner {
 
     public static void main(String[] args) {
-        MyNumber number = new MyNumber(3);
+        MyNumber number = new MyNumber(9);
         System.out.println(number.isPrime());
+        int sum = number.sumUptoN();
+        System.out.println(sum);
+        int divSum = number.sumOfDivisors();
+        System.out.println(divSum);
     }
 
 }
