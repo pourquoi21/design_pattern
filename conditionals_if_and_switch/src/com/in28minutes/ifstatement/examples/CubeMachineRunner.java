50d674ac7f975dacf622c4c7142b819a12b75f69
pourquoi21 <miyaku219@naver.com>
2023-07-31 23:57:42 +0900

    making new one

---
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/CubeMachineRunner.java
@@ -0,0 +1,13 @@
+package com.in28minutes.ifstatement.examples;
+
+import java.util.Scanner;
+
+public class CubeMachineRunner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CubeMachine cubeMachine = new CubeMachine();
+    }
+
+}
