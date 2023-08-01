bff403342a73ce3e408cf8bb8a9b516a826b769a
pourquoi21 <miyaku219@naver.com>
2023-08-01 23:37:21 +0900

    modify

--- conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/CubeMachineRunner.java
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/CubeMachineRunner.java
@@ -6,8 +6,16 @@ public class CubeMachineRunner {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number;
+        do {
+            System.out.print("Enter a number: ");
 
-        CubeMachine cubeMachine = new CubeMachine();
+            number = scanner.nextInt();
+
+            System.out.println("Cube is " + number * number * number);
+
+        } while (number > 0);
+        System.out.println("Thank You! Have Fun!");
     }
 
 }
