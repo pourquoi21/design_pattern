37c5db65964f6e43b6ae82ae8fb2ad499fc11dc4
pourquoi21 <miyaku219@naver.com>
2023-08-09 23:59:31 +0900

    name.. not yet

--- conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/Animal.java
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/Animal.java
@@ -2,9 +2,11 @@ package com.in28minutes.ifstatement.examples;
 
 public class Animal {
     int id;
+    String name;
 
     Animal(int id) {
         this.id = id;
+
     }
 
     public void cry() {
