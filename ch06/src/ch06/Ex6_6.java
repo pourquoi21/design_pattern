6566190f269d4097ee3e8ddbf246320f1f6004bb
pourquoi21 <miyaku219@naver.com>
2023-10-14 23:08:53 +0900

    about parameter

---
+++ ch06/src/ch06/Ex6_6.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class Ex6_6 {
+
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);
+
+    }
+
+}
+
+class Data {
+    int x;
+}
