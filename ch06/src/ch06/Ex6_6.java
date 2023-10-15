5648404d7502f3bd7847eb98a08af6e60dc1ff2f
pourquoi21 <miyaku219@naver.com>
2023-10-15 23:33:49 +0900

    scope of variable

--- ch06/src/ch06/Ex6_6.java
+++ ch06/src/ch06/Ex6_6.java
@@ -7,6 +7,14 @@ public class Ex6_6 {
         d.x = 10;
         System.out.println("main() : x = " + d.x);
 
+        change(d.x);
+        System.out.println("after change main() : x = " + d.x);
+
+    }
+
+    static void change(int x) {
+        x = 1000;
+        System.out.println("change() : x = " + x);
     }
 
 }
