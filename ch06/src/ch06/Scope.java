b11a9d6d52dbcded8a853433f81da05d18493731
pourquoi21 <miyaku219@naver.com>
2023-10-16 08:52:52 +0900

    variables

--- ch06/src/ch06/Scope.java
+++ ch06/src/ch06/Scope.java
@@ -23,6 +23,10 @@ public class Scope {
         System.out.println("add2 third: " + add2());
         System.out.println(s.add(a, b));
         System.out.println(s.add2()); // s의 a와 b를 주석처리했으므로 0나옴
+
+        change(s);
+        System.out.println("after change(s) : a = " + s.a);
+        System.out.println("after change(s) add = " + s.add(a, b));
     }
 
     String add(int a, int b) {
@@ -34,6 +38,12 @@ public class Scope {
     static String add2() {
         int c = a + b;
         return String.valueOf(c);
+    }
+
+    static void change(Scope s) {
+        s.a = 2000;
+        System.out.println("change() : a = " + s.a);
+
     }
 
 }
