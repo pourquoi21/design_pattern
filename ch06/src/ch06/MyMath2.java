425f209c4b234cafe72c4723f159eda8a9ca8590
pourquoi21 <miyaku219@naver.com>
2023-10-26 23:54:52 +0900

    commit..

--- ch06/src/ch06/MyMath2.java
+++ ch06/src/ch06/MyMath2.java
@@ -1,13 +1,15 @@
 package ch06;
 
 public class MyMath2 {
-    long a, b;
+	long a, b;
 
-    long add() { // instance method
-        return a + b; // iv 사용
-    }
+	long add() { // instance method
+		return a + b; // iv 사용
+	}
 
-    static long add(long a, long b) { // static method
-        return a + b; // iv 사용X
-    }
+	static long add(long a, long b) { // static method
+		return a + b; // iv 사용X
+
+		// 메서드가 종료될때까지가 long a와 long b의 유효범위(lv이므로)
+	}
 }
