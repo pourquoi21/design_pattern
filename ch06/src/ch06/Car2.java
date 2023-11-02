8dd433825a856d965a47a1b9608662dbcd599ad9
pourquoi21 <miyaku219@naver.com>
2023-11-02 23:16:50 +0900

    constructor this()

---
+++ ch06/src/ch06/Car2.java
@@ -0,0 +1,22 @@
+package ch06;
+
+public class Car2 {
+	String color;
+	String gearType;
+	int door;
+
+	Car2() {
+		this("white", "auto", 4);
+	}
+
+	Car2(String color) {
+		this(color, "auto", 4);
+	}
+
+	Car2(String color, String gearType, int door) {
+		this.color = color;
+		this.gearType = gearType;
+		this.door = door;
+	}
+
+}
