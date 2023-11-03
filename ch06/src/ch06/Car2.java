507f991fa554270e5e1410f98102b753faea6478
pourquoi21 <miyaku219@naver.com>
2023-11-03 22:18:28 +0900

    about constructor this

--- ch06/src/ch06/Car2.java
+++ ch06/src/ch06/Car2.java
@@ -5,6 +5,9 @@ public class Car2 {
 	String gearType;
 	int door;
 
+	// 생성자에서 같은 클래스의 다른 생성자를 호출할 때 this를 쓴다.
+	// this를 쓸 때는 해당 생성자메서드 내의 첫번째 줄에 써야 한다.
+
 	Car2() {
 		this("white", "auto", 4);
 	}
