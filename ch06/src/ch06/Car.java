ff4a1b4f7398e551f532efca519fb554a16be352
pourquoi21 <miyaku219@naver.com>
2023-11-05 22:57:35 +0900

    avoiding repetitive codes

--- ch06/src/ch06/Car.java
+++ ch06/src/ch06/Car.java
@@ -6,6 +6,14 @@ public class Car {
 	int door;
 
 	Car() {
+//		color = "white";
+//		gearType = "auto";
+//		door = 4;
+
+		// 위의 코드를 아래와 같이 중복제거
+
+		this("white", "auto", 4);
+
 	}// 기본 생성자
 
 	Car(String c, String g, int d) {
