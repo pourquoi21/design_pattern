0393c453ab41f115df652ab25c8d8237d6217f32
pourquoi21 <miyaku219@naver.com>
2023-11-09 00:34:40 +0900

    reference variable this final

--- ch06/src/ch06/MyMath2.java
+++ ch06/src/ch06/MyMath2.java
@@ -4,7 +4,7 @@ public class MyMath2 {
 	long a, b; // this.a, this.b가 해당 iv의 진짜 이름.
 
 	MyMath2(int a, int b) { // 생성자
-		this.a = a;
+		this.a = a; // lv와 iv 구분위해 this사용
 		this.b = b;
 	}
 
@@ -13,7 +13,7 @@ public class MyMath2 {
 	}
 
 	static long add(long a, long b) { // static method
-		return a + b; // iv 사용X
+		return a + b; // iv 사용X, this 사용불가
 
 		// 메서드가 종료될때까지가 long a와 long b의 유효범위(lv이므로)
 	}
