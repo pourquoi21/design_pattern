5dcb937163042ae4667e563ef6cd8f5332f0a1a4
pourquoi21 <miyaku219@naver.com>
2023-11-17 00:35:31 +0900

    quiz

--- ch06/src/ch07/solution.js
+++ ch06/src/ch07/solution.js
@@ -12,10 +12,23 @@
                     break;
                 } else if (!numbers[j]) {
                     answer.push(-1);
-                } else {
-                    continue;
                 }
             }
+        }
+        
+     // gpt의 제안(그러나 정답은 아닌)
+     for (let i = 0; i < numbers.length; i++) {
+           let found = false;
+            for (let j = i+1; j < numbers.length; j++) {
+                if (numbers[i] < numbers[j]) {
+                    answer.push(numbers[j]);
+                    found = true;
+                    break;
+                } 
+            }
+           if (!found) {
+               answer.push(-1);
+           }
         }
     
     return answer;
