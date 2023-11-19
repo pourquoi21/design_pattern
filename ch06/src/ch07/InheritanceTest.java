ad60b127fc470d49530cd1c3439ad7e753a86302
pourquoi21 <miyaku219@naver.com>
2023-11-19 23:53:16 +0900

    inheritance

---
+++ ch06/src/ch07/InheritanceTest.java
@@ -0,0 +1,25 @@
+package ch07;
+
+class Point {
+	int x;
+	int y;
+}
+
+class Circle extends Point { // 상속
+	int r;
+}
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		Circle c = new Circle();
+		c.x = 1;
+		c.y = 2;
+		c.r = 3;
+
+		System.out.println("c.x = " + c.x);
+		System.out.println("c.y = " + c.y);
+		System.out.println("c.r = " + c.r);
+	}
+
+}
