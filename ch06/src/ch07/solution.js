f0a3ec3ba8210cacbabef858dfb5eeff10271bbe
pourquoi21 <miyaku219@naver.com>
2023-11-24 23:47:19 +0900

    Hmm

--- ch06/src/ch07/solution.js
+++ ch06/src/ch07/solution.js
@@ -66,6 +66,67 @@
           
         }
     answer.reverse();
+    
+    // 너무 길어서인지 런타임에러남 아
+    let temp = 0;
+    let tempLarge = 0;
+    let copiedArr = [...numbers];
+    let i = numbers.length - 1;
+    
+       while (i >= 0) {
+           let found = false;
+ 
+           if (i == numbers.length - 1) {
+               temp = copiedArr.pop();
+               answer.push(-1);
+               found = true;
+               i--;
+               continue;
+           } else if (temp < copiedArr[i]) {
+               if (temp < tempLarge) {
+                   answer.push(tempLarge);
+                   continue;
+               }
+               temp = copiedArr.pop();
+           } else if (temp > copiedArr[i]) {
+               if (temp > copiedArr[0]) {
+                   tempLarge = temp;
+               }
+               if(copiedArr[i] < numbers[copiedArr.length]) {
+                    temp = numbers[copiedArr.length];
+                }
+               answer.push(temp); 
+               copiedArr.pop();
+               found = true;
+           }
+
+           if (!found) {
+               answer.push(-1);
+           }
+           i--;
+        }
+    answer.reverse();
+    
+    // gpt가 알려준 방법 + for문을 while로 (그러나 시간초과로 통과 못함)
+     let tmp = [];
+    let i = numbers.length - 1;
+    
+    while (i >= 0) {
+        
+        while (tmp.length > 0 && numbers[i] >= tmp[tmp.length - 1]) {
+            tmp.pop();
+        }
+        
+        if (tmp.length == 0) {
+            answer.unshift(-1);
+        } else {
+            answer.unshift(tmp[tmp.length - 1]);
+        }
+
+        tmp.push(numbers[i]);
+        
+        i--;
+    }
   	
     
     return answer;
