05a481fee059be15f6a77d363252a8206de4e171
pourquoi21 <miyaku219@naver.com>
2023-12-18 21:44:08 +0900

    access modifier2

--- ch06/src/ch07/MyParent.java
+++ ch06/src/ch07/MyParent.java
@@ -1,6 +1,6 @@
 package ch07;
 
-class MyParent {
+public class MyParent {
 	private int prv; // 같은 클래스
 	int dft; // 같은 패키지
 	protected int prt; // 같은 패키지 + 자손(다른 패키지)
@@ -14,10 +14,10 @@ class MyParent {
 	}
 }
 
-public class MyParentTest {
+class MyParentTest {
 	public static void main(String[] args) {
 		MyParent p = new MyParent();
-		System.out.println(p.prv); // 에러
+//		System.out.println(p.prv); // 에러
 		System.out.println(p.dft);
 		System.out.println(p.prt);
 		System.out.println(p.pub);
---
+++ ch06/src/com/codechobo/book/MyParentTest2.java
@@ -0,0 +1,21 @@
+package com.codechobo.book;
+
+import ch07.MyParent;
+
+class MyChild extends MyParent {
+	public void printMembers() {
+		System.out.println(prv);
+		System.out.println(dft);
+		System.out.println(prt);
+		System.out.println(pub);
+	}
+}
+
+public class MyParentTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
