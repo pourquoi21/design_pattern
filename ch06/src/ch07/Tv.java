ca75c41071c12221076bfef5c3e9844132877199
pourquoi21 <miyaku219@naver.com>
2023-12-24 23:54:49 +0900

    poly ing..

--- ch06/src/ch07/Tv.java
+++ ch06/src/ch07/Tv.java
@@ -23,5 +23,8 @@ class SmartTv extends Tv {
 
 	public static void main(String[] args) {
 		Tv t = new SmartTv(); // 다형성: 타입 불일치
+		// 자손은 조상보다 많은 멤버를 갖고있다.
+		// 이렇게 타입 불일치하게 선언했을 때는 자손의 모든 멤버를 사용할 수 없을 텐데,
+		// 다형성에 장점이 있는 이유는..?
 	}
 }
