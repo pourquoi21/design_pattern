33fda6e25683b7b60551e3313f3aea54f4d89bb6
pourquoi21 <miyaku219@naver.com>
2023-12-30 23:38:40 +0900

    NullPointerException

--- ch06/src/ch07/Ex7_7.java
+++ ch06/src/ch07/Ex7_7.java
@@ -6,6 +6,14 @@ public class Ex7_7 {
 		Car car = null;
 		FireEngine fe = new FireEngine();
 		FireEngine fe2 = null;
+		FireEngine fe3 = (FireEngine) car; // 조상 -> 자손으로 형변환
+		Car car2 = (Car) fe2; // 자손 -> 조상으로 형변환
+
+		// 주의할점
+		Car car3 = null; // null이어도 형변환에는 문제가 없지만
+		FireEngine fe4 = (FireEngine) car3;
+		fe4.water(); // 여기서 nullpointerexception나옴
+		car3.drive();
 
 		fe.water();
 		car = fe; // car = (Car)fe;에서 형변환이 생략됨
