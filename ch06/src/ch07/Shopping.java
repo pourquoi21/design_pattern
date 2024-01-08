7fc737e6b01534077c5a6d5bbff1a00e7820a274
pourquoi21 <miyaku219@naver.com>
2024-01-08 23:40:19 +0900

    perchase

--- ch06/src/ch07/Shopping.java
+++ ch06/src/ch07/Shopping.java
@@ -12,6 +12,9 @@ public class Shopping {
 		// 그 자손인 tv와 com을 받는 데에 문제가 없다.
 		b.buy(tv);
 		b.buy(com);
+
+		System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
+		System.out.println("현재 마일리지는 " + b.bonusPoint + "입니다.");
 	}
 
 }
@@ -22,20 +25,40 @@ class Product {
 
 	Product(int price) {
 		this.price = price;
-		bonusPoint = (int) (price / 10.0);
+		bonusPoint = (int) (price / 10.0); // 보너스점수는 제품가격의 10%
 	}
 }
 
 class Tv1 extends Product {
 	Tv1() {
-		super(100);
+		// 조상클래스의 생성자 Product(int price)를 호출한다.
+		super(100); // Tv의 가격을 100만원으로 한다.
+	}
+
+	// Object클래스의 toString()을 오버라이딩한다.
+	public String toString() {
+		return "Tv";
 	}
 }
 
 class Computer extends Product {
+	Computer() {
+		super(200);
+	}
+
+	public String toString() {
+		return "Computer";
+	}
 }
 
 class Audio extends Product {
+	Audio() {
+		super(150);
+	}
+
+	public String toString() {
+		return "Audio";
+	}
 }
 
 class Buyer { // 물건사는 사람
@@ -51,5 +74,8 @@ class Buyer { // 물건사는 사람
 	void buy(Product p) {
 		money -= p.price;
 		bonusPoint += p.bonusPoint;
+
+		System.out.println(p + "을/를 구입하셨습니다.");
+
 	}
 }
