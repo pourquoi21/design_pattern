cd7980feb69179494d43c6de67b3b23f782447f3
pourquoi21 <miyaku219@naver.com>
2024-01-09 23:37:14 +0900

    shopping end

--- ch06/src/ch07/Shopping.java
+++ ch06/src/ch07/Shopping.java
@@ -10,8 +10,12 @@ public class Shopping {
 
 		// buy메서드 선언시 인자를 Product타입으로 했기에
 		// 그 자손인 tv와 com을 받는 데에 문제가 없다.
-		b.buy(tv);
-		b.buy(com);
+//		b.buy(tv);
+//		b.buy(com);
+
+		// 이렇게 해도 된다!
+		b.buy(new Tv1());
+		b.buy(new Computer());
 
 		System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
 		System.out.println("현재 마일리지는 " + b.bonusPoint + "입니다.");
@@ -72,9 +76,15 @@ class Buyer { // 물건사는 사람
 
 	// 참조형 매개변수는 메서드 호출시 자신과 같은 타입 또는 자손타입의 인스턴스를 넘겨줄 수 있다.
 	void buy(Product p) {
+
+		if (money < p.price) {
+			System.out.println("잔액이 부족하여 구입할 수 없습니다.");
+			return;
+		}
 		money -= p.price;
 		bonusPoint += p.bonusPoint;
 
+		// 참조변수와 문자열 결합할 때는 변수.toString()써야 하는데 우리는 이미 선언해주었음
 		System.out.println(p + "을/를 구입하셨습니다.");
 
 	}
