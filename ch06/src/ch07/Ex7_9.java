9f84c72435670e781445302c28568c23e603645b
pourquoi21 <miyaku219@naver.com>
2024-01-16 23:46:21 +0900

    product2

---
+++ ch06/src/ch07/Ex7_9.java
@@ -0,0 +1,16 @@
+package ch07;
+
+public class Ex7_9 {
+
+}
+
+class Product2 {
+	int price;
+	int bonusPoint;
+
+	Product2(int price) {
+		this.price = price;
+		bonusPoint = (int) (price / 10.0);
+	}
+
+}
