38b342cb7758d5169b821a6216b8a1a5b45919b1
pourquoi21 <miyaku219@naver.com>
2024-01-17 23:53:49 +0900

    shopping

--- ch06/src/ch07/Ex7_9.java
+++ ch06/src/ch07/Ex7_9.java
@@ -13,4 +13,56 @@ class Product2 {
 		bonusPoint = (int) (price / 10.0);
 	}
 
+	Product2() {
+	} // 기본 생성자
+
+}
+
+class Tv2 extends Product2 {
+	Tv2() {
+		super(100);
+	}
+
+	public String toString() {
+		return "Tv";
+	}
+}
+
+class Computer2 extends Product2 {
+	Computer2() {
+		super(200);
+	}
+
+	public String toString() {
+		return "Computer";
+	}
+}
+
+class Audio2 extends Product2 {
+	Audio2() {
+		super(50);
+	}
+
+	public String toString() {
+		return "Audio";
+	}
+}
+
+class Buyer2 {
+	int money = 1000;
+	int bonusPoint = 0;
+	Product2[] cart = new Product2[10];
+	int i = 0;
+
+	void buy(Product2 p) {
+		if (money < p.price) {
+			System.out.println("잔액이 부족하여 물건을 살 수 없습니다.");
+			return;
+		}
+
+		money -= p.price;
+		bonusPoint += p.bonusPoint;
+		cart[i++] = p;
+		System.out.println(p + "을/를 구입하셨습니다.");
+	}
 }
