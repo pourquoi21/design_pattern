8b82cdce7c1d212986eac9ef88d643b8fec57a3d
pourquoi21 <miyaku219@naver.com>
2024-01-27 23:24:59 +0900

    inheritance of an abstract class

--- ch06/src/ch07/Player.java
+++ ch06/src/ch07/Player.java
@@ -9,5 +9,16 @@ public abstract class Player { // 추상클래스(미완성 클래스)
 		// 추상클래스는 다른 클래스 작성에 도움을 주기 위한 것이므로
 		// 인스턴스 생성은 불가
 		Player p = new Player(); // 에러: 추상클래스의 인스턴스 생성 불가
+
+		AudioPlayer ap = new AudioPlayer(); // 이건 가능
 	}
+}
+
+// 상속을 통해 추상메서드를 완성해야 인스턴스 생성 가능
+class AudioPlayer extends Player {
+	void play(int pos) {
+	}; // 추상메서드를 구현
+
+	void stop() {
+	}; // 추상메서드를 구현
 }
