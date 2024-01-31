616316fa944a3c23ad0aad499f347968d0540e50
pourquoi21 <miyaku219@naver.com>
2024-01-31 23:38:28 +0900

    ing..

--- ch06/src/ch07/Player.java
+++ ch06/src/ch07/Player.java
@@ -1,6 +1,10 @@
 package ch07;
 
 public abstract class Player { // 추상클래스(미완성 클래스)
+
+	boolean pause;
+	int currentPos;
+
 	abstract void play(int pos); // 추상메서드(몸통{}이 없는 미완성 메서드)
 
 	abstract void stop(); // 추상메서드
