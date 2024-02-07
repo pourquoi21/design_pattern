bbbae4955a0103c669106193068d955e272c3f4d
pourquoi21 <miyaku219@naver.com>
2024-02-07 22:56:30 +0900

    playerTest

---
+++ ch06/src/ch07/PlayerTest.java
@@ -0,0 +1,14 @@
+package ch07;
+
+class Player1 {
+	void play(int pos); // 일반 메서드로 지정했는데 몸통이 없으니까 에러난다.
+}
+
+public class PlayerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
