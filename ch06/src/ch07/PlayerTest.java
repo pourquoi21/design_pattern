a97f89711f9949457a2ee8b1b3ffba48d38c1547
pourquoi21 <miyaku219@naver.com>
2024-02-08 23:45:46 +0900

    abstract

--- ch06/src/ch07/PlayerTest.java
+++ ch06/src/ch07/PlayerTest.java
@@ -1,7 +1,8 @@
 package ch07;
 
 class Player1 {
-	void play(int pos); // 일반 메서드로 지정했는데 몸통이 없으니까 에러난다.
+//	void play(int pos); // 일반 메서드로 지정했는데 몸통이 없으니까 에러난다.
+	abstract void play(int pos); // 이렇게 해도 에러난다: 클래스에 abstract안붙여서
 }
 
 public class PlayerTest {
