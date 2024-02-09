08418645c762e2e396e8fe7f25ff4c221a95fa82
pourquoi21 <miyaku219@naver.com>
2024-02-09 23:47:52 +0900

    a bit

--- ch06/src/ch07/PlayerTest.java
+++ ch06/src/ch07/PlayerTest.java
@@ -1,6 +1,6 @@
 package ch07;
 
-class Player1 {
+abstract class Player1 { // 추상메서드 가지고있다면 추상클래스로
 //	void play(int pos); // 일반 메서드로 지정했는데 몸통이 없으니까 에러난다.
 	abstract void play(int pos); // 이렇게 해도 에러난다: 클래스에 abstract안붙여서
 }
