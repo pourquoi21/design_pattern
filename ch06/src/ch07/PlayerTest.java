0c12e2535ee0f3c1c48f77d4fdca493e8cfba05f
pourquoi21 <miyaku219@naver.com>
2024-02-10 23:51:54 +0900

    without the content, make it as abstract

--- ch06/src/ch07/PlayerTest.java
+++ ch06/src/ch07/PlayerTest.java
@@ -3,6 +3,8 @@ package ch07;
 abstract class Player1 { // 추상메서드 가지고있다면 추상클래스로
 //	void play(int pos); // 일반 메서드로 지정했는데 몸통이 없으니까 에러난다.
 	abstract void play(int pos); // 이렇게 해도 에러난다: 클래스에 abstract안붙여서
+
+	abstract void stop(); // 몸통없는 클래스 만들고 싶으면 abstract로!
 }
 
 public class PlayerTest {
