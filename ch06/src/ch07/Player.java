2e39c6ab42bcb4f66a9583567b477343f18fd625
pourquoi21 <miyaku219@naver.com>
2024-02-20 23:43:37 +0900

    example

--- ch06/src/ch07/Player.java
+++ ch06/src/ch07/Player.java
@@ -24,7 +24,7 @@ public abstract class Player { // 추상클래스(미완성 클래스)
 	public static void main(String[] args) {
 		// 추상클래스는 다른 클래스 작성에 도움을 주기 위한 것이므로
 		// 인스턴스 생성은 불가
-		Player p = new Player(); // 에러: 추상클래스의 인스턴스 생성 불가
+//		Player p = new Player(); // 에러: 추상클래스의 인스턴스 생성 불가
 
 		AudioPlayer ap = new AudioPlayer(); // 이건 가능
 		Player ap2 = new AudioPlayer(); // 이것도 가능(자손이니까), 조상은 변수만 제공하고있는 것
