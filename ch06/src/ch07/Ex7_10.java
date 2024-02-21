49004aa83fb81b01c72fbbf9ffbe1ae1fee0ff63
pourquoi21 <miyaku219@naver.com>
2024-02-21 23:18:34 +0900

    moving

--- ch06/src/ch07/Ex7_10.java
+++ ch06/src/ch07/Ex7_10.java
@@ -3,8 +3,13 @@ package ch07;
 public class Ex7_10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Unit[] group = { new Marine(), new Tank(), new Dropship() };
 
+		for (int i = 0; i < group.length; i++) {
+			group[i].move(100, 200);
+		}
+
+//		group[2].load();
 	}
 
 }
@@ -16,4 +21,34 @@ abstract class Unit {
 
 	void stop() {
 		/* 현재 위치에 정지 */}
+}
+
+class Marine extends Unit {
+	void move(int x, int y) {
+		System.out.println("Marine[x = " + x + ", y = " + y + "]");
+	}
+
+	void stickPack() {
+		/* 스팀팩을 사용한다 */}
+}
+
+class Tank extends Unit {
+	void move(int x, int y) {
+		System.out.println("Tank[x = " + x + ", y = " + y + "]");
+	}
+
+	void changeMode() {
+		/* 공격모드 변환 */}
+}
+
+class Dropship extends Unit {
+	void move(int x, int y) {
+		System.out.println("Dropship[x = " + x + ", y = " + y + "]");
+	}
+
+	void load() {
+	}
+
+	void unload() {
+	}
 }
