4e6a547c2376934bf462c43252fc7abca66b539b
pourquoi21 <miyaku219@naver.com>
2024-02-27 23:57:55 +0900

    example

---
+++ ch06/src/ch07/PlayingCard1.java
@@ -0,0 +1,12 @@
+package ch07;
+
+public interface PlayingCard1 {
+	public static final int SPADE = 4;
+	final int DIAMOND = 3;
+	static int HEART = 2;
+	int CLOVER = 1;
+
+	public abstract String getCardNumber1();
+
+	String getCardKind1();
+}
