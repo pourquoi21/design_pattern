cc39035f0d452707a6ab8cf5474ecd47172fea88
pourquoi21 <miyaku219@naver.com>
2024-03-06 23:52:15 +0900

    interface

--- ch06/src/ch07/Fighter1.java
+++ ch06/src/ch07/Fighter1.java
@@ -8,6 +8,10 @@ public class Fighter1 extends Unit implements Fightable1 {
 	public void attack(Unit u) {
 	};
 
+	public static void main(String[] args) {
+		Fightable1 f = new Fighter1();
+		// 인터페이스일 경우에도 조상클래스로 자손객체 가리킬수있다.
+	}
 	// 일부만 구현하는 경우 클래스 앞에 abstract 붙여야함.
 }
 
