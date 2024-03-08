e56a800a9f6b26c0884288add391f9c7f6af3dd3
pourquoi21 <miyaku219@naver.com>
2024-03-08 23:59:55 +0900

    hm

--- ch06/src/ch07/Fighter1.java
+++ ch06/src/ch07/Fighter1.java
@@ -14,10 +14,18 @@ public class Fighter1 extends Unit implements Fightable1 {
 		// 인터페이스일 경우에도 조상클래스로 자손객체 가리킬수있다.
 	}
 	// 일부만 구현하는 경우 클래스 앞에 abstract 붙여야함.
+
+	@Override
+	public void attack(Fightable1 f) {
+		// TODO Auto-generated method stub
+
+	}
 }
 
 interface Fightable1 {
 	void move(int x, int y);
 
-	void attack(Unit u);
+//	void attack(Unit u);
+	void attack(Fightable1 f);
+	// 인터페이스 타입 매개변수는 인터페이스를 구혀난 클래스의 객체만 가능
 }
