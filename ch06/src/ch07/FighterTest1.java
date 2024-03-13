f859e8d248e7b208b96ba685927f03c6c6d974fd
pourquoi21 <miyaku219@naver.com>
2024-03-13 23:34:41 +0900

    example

---
+++ ch06/src/ch07/FighterTest1.java
@@ -0,0 +1,20 @@
+package ch07;
+
+abstract class Unit1 {
+	int x, y;
+
+	abstract void move(int x, int y);
+
+	void stop() {
+		System.out.println("멈춥니다.");
+	}
+}
+
+public class FighterTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
