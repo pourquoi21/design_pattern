0857792419ea04691a01cee4cd0ed9d603e4da49
pourquoi21 <miyaku219@naver.com>
2024-03-18 21:00:58 +0900

    test

--- ch06/src/ch07/FighterTest1.java
+++ ch06/src/ch07/FighterTest1.java
@@ -11,25 +11,41 @@ abstract class Unit1 {
 	}
 }
 
-interface Fightable2 {
+interface Fightable2 { // 인터페이스의 모든 메서드는 public abstract임
 	void move(int x, int y); // public abstract 생략됨
 
-	void attack(Fightable2 f);
+	void attack(Fightable2 f); // public abstract 생략됨
 }
 
 class Fighter2 extends Unit1 implements Fightable2 {
-	void move(int x, int y) {
+	// 오버라이딩 규칙: 조상(public)보다 접근제어자가 좁으면 안된다.
+	// public 안붙이면 default이므로 안된다.
+	public void move(int x, int y) {
+		System.out.println("[" + x + ", " + y + "] 로 이동");
 	}
 
-	void attack(Fightable2 f) {
+	public void attack(Fightable2 f) {
+		System.out.println(f + "를 공격");
 	};
+
+	// 싸울 수 있는 상대를 불러온다.
+	Fightable2 getFightable() {
+		Fighter2 f = new Fighter2(); // Fighter를 생성해서 반환
+		return f;
+	}
 }
 
 public class FighterTest1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		Fighter2 f = new Fighter2();
+//		Unit1 f = new Fighter2(); // Unit1에는 attack()이 없어서 호출불가 
+//		Fightable2 f = new Fighter2();
+//		f.move(100, 200);
+//		f.attack(new Fighter2());
+		Fighter2 f = new Fighter2();
 
+		Fightable2 f2 = f.getFightable();
 	}
 
 }
