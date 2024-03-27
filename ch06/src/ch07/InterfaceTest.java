d8a3575344acf782365310c8b886d6a68f34f3ea
pourquoi21 <miyaku219@naver.com>
2024-03-27 23:56:56 +0900

    changing parameter

--- ch06/src/ch07/InterfaceTest.java
+++ ch06/src/ch07/InterfaceTest.java
@@ -23,6 +23,12 @@ public class InterfaceTest {
 	public static void main(String[] args) {
 		A a = new A();
 		a.method(new B()); // A가 B를 사용(의존)
+
+		// C클래스를 인자로 쓰고 싶으면
+//		a.method(new C());
+
+		// 이렇게 인자만 바꾸는 게 아니라
+//		class A의 method선언부에도 바꿔줘야한다.
 	}
 
 }
