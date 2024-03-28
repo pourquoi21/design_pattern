268a44da5966bc71274987c5bc29915703e2269d
pourquoi21 <miyaku219@naver.com>
2024-03-28 23:46:44 +0900

    interface

--- ch06/src/ch07/InterfaceTest.java
+++ ch06/src/ch07/InterfaceTest.java
@@ -1,20 +1,38 @@
 package ch07;
 
+//class A {
+//	public void method(B b) {
+//		b.method();
+//	}
+//}
+
+// 선언과 구현을 분리한 후의 인자
 class A {
-	public void method(B b) {
-		b.method();
+	public void method(I i) { // interface I를 구현한 녀석만 들어올 수 있음.
+		i.method();
 	}
 }
 
-class B {
+//class B {
+//	public void method() {
+//		System.out.println("B클래스의 메서드");
+//	}
+//}
+
+// B클래스의 선언과 구현을 분리
+interface I {
+	public void method();
+}
+
+class B implements I {
 	public void method() {
 		System.out.println("B클래스의 메서드");
 	}
 }
 
-class C1 {
+class C1 implements I {
 	public void method() {
-
+		System.out.println("C클래스의 메서드");
 	}
 }
 
@@ -23,6 +41,7 @@ public class InterfaceTest {
 	public static void main(String[] args) {
 		A a = new A();
 		a.method(new B()); // A가 B를 사용(의존)
+		a.method(new C1());
 
 		// C클래스를 인자로 쓰고 싶으면
 //		a.method(new C());
