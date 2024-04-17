c4f29d181eb593b3009b4f260a0b38332c973775
pourquoi21 <miyaku219@naver.com>
2024-04-17 23:56:20 +0900

    stack

--- ch06/src/ch07/MyStack.java
+++ ch06/src/ch07/MyStack.java
@@ -1,13 +1,18 @@
 package ch07;
 
 public class MyStack<E> {
-	private static final int DEFAULT_CAPACITY = 6;
-	private Object[] arr;
-	private int top;
+	private static final int DEFAULT_CAPACITY = 6; // 최소 용량 크기
+	private Object[] arr; // 요소를 담을 내부 배열
+	private int top; // 스택의 가장 마지막 요소를 가리키는 포인터
 
 	// 생성자
 	public MyStack() {
-		this.arr = new Object[DEFAULT_CAPACITY];
+		this.arr = new Object[DEFAULT_CAPACITY]; // 6 용량으로 내부 배열 생성
 		this.top = -1;
+	}
+
+	public boolean isFull() {
+		// 마지막 요소 위치인 top이 배열 길이 - 1 과 같은 경우 끝까지 차있는 것과 같음.
+		return top == arr.length - 1;
 	}
 }
--- ch06/src/ch07/Solution.java
+++ ch06/src/ch07/Solution.java
@@ -393,7 +393,6 @@ public class Solution {
 	public long move(int a, int b, int[] g, int[] s, int[] w, int[] t) {
 		long left = 0;
 		long right = (long) (4 * Math.pow(10, 14));
-		System.out.println(right);
 
 		while (left + 1 < right) {
 			long mid = (left + right) / 2;
@@ -407,54 +406,6 @@ public class Solution {
 		System.out.println(left);
 		return left;
 	}
-
-	// 특정 시간이 주어졌을 떄, (a, b)만큼 조달이 가능한지 리턴
-	// 가능하면 ture
-	// 불가능하면 false
-//	public boolean isPossible(long time, int a, int b, int[] g, int[] s, int[] w, int[] t) {
-//		int n = g.length;
-//		long total = 0L;
-//		long totalG = 0L;
-//		long totalS = 0L;
-//
-//		for (int i = 0; i < n; i++) {
-//			// 해당 시간에 옮길 수 있는 횟수
-//			long cnt = time / (2L * t[i]);
-//			if (time % (2L * t[i]) >= t[i])
-//				cnt++;
-//
-//			// 해당 시간에 옮길 수 있는 무게
-//			long tmp = Math.min(cnt * w[i], g[i] + s[i]);
-//			// 각 도시의 총합 최대 무게 누적
-//			total += tmp;
-//			// 각 도시의 금 최대 무게 누적
-//			totalG += Math.min(tmp, g[i]);
-//			// 각 도시의 은 최대 무게 누적
-//			totalS += Math.min(tmp, s[i]);
-//		}
-//
-//		if (total >= a + b && totalG >= a && totalS >= b)
-//			return true;
-//		return false;
-//	}
-//
-//	public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-//		long hi = 400000000000000L;
-//		long lo = 0;
-//
-//		// 이분 탐색
-//		while (lo + 1 < hi) {
-//			long mid = (lo + hi) / (long) 2;
-//
-//			if (isPossible(mid, a, b, g, s, w, t))
-//				hi = mid;
-//			else
-//				lo = mid;
-//		}
-//
-//		System.out.println("lo :: " + lo + " hi:: " + hi);
-//		return hi;
-//	}
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
