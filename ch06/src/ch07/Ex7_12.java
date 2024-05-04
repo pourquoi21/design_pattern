04c127e86c3b836cd857f0750bafc9d580f2d161
pourquoi21 <miyaku219@naver.com>
2024-05-04 23:50:47 +0900

    about inner class

--- ch06/src/ch07/Ex7_12.java
+++ ch06/src/ch07/Ex7_12.java
@@ -1,6 +1,7 @@
 package ch07;
 
 class Ex7_12 {
+
 	class InstanceInner {
 		int iv = 100;
 		static int cv = 100; // static class가 아닌데 왜 static 변수 쓰는게 가능하지?
@@ -20,9 +21,16 @@ class Ex7_12 {
 			static int cv = 300;
 			final static int CONST = 300; // 상수는 허용
 		}
+
+		int i = LocalInner.CONST; // 이건 되지만
 	}
 
 	public static void main(String[] args) {
 		System.out.println(InstanceInner.cv);
+		System.out.println(StaticInner.cv);
+		// localInner의 변수를 이렇게 외부에서 쓸 수는 없다.
+		// 왜냐하면 localInner는 method안에서만 사용되고 사라지기 때문
+		System.out.println(LocalInner.CONST);
+
 	}
 }
