10ffca8ff8b51dbf36450b32584c3efea4d29365
pourquoi21 <miyaku219@naver.com>
2024-05-14 23:26:13 +0900

    private

--- ch06/src/ch07/Outer1.java
+++ ch06/src/ch07/Outer1.java
@@ -3,4 +3,9 @@ package ch07;
 public class Outer1 {
 	private int outerIv = 0;
 	static int outerCv = 0;
+
+	class InstanceInner1 {
+		int iiv = outerIv; // 외부 클래스의 private멤버에도 접근 가능
+		int iiv2 = outerCv;
+	}
 }
