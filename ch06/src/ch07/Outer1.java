172f6bc9630ea3a4269c7a8c528e4a30d2d4a106
pourquoi21 <miyaku219@naver.com>
2024-05-15 23:52:57 +0900

    static & instance member

--- ch06/src/ch07/Outer1.java
+++ ch06/src/ch07/Outer1.java
@@ -8,4 +8,9 @@ public class Outer1 {
 		int iiv = outerIv; // 외부 클래스의 private멤버에도 접근 가능
 		int iiv2 = outerCv;
 	}
+
+	static class StaticInner {
+		int siv = outerIv; // static 클래스는 외부클래서의 인스턴스멤버에 접근불가능
+		static int scv = outerCv; // 그러나 외부클래스의 스태틱멤버에는 접근가능
+	}
 }
