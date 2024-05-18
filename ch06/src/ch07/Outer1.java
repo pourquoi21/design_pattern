bb2d0d5a9c743b22fa17a5fcd6e2326eb103d1aa
pourquoi21 <miyaku219@naver.com>
2024-05-18 23:56:05 +0900

    local

--- ch06/src/ch07/Outer1.java
+++ ch06/src/ch07/Outer1.java
@@ -10,7 +10,22 @@ public class Outer1 {
 	}
 
 	static class StaticInner {
-		int siv = outerIv; // static 클래스는 외부클래서의 인스턴스멤버에 접근불가능
+		int siv = outerIv; // static 클래스는 외부클래스의 인스턴스멤버에 접근불가능
 		static int scv = outerCv; // 그러나 외부클래스의 스태틱멤버에는 접근가능
+	}
+
+	void myMethod() {
+		// 지역내부클래스에서의 변수와 상수
+		int lv = 0;
+		final int LV = 0; // JDK1.8부터 final 생략가능
+
+		class LocalInner1 {
+			int liv = outerIv;
+			int liv2 = outerCv;
+
+			// 외부클래스의 지역변수는 final이 붙은 변수(상수)만 접근가능
+			int liv3 = lv; // 에러였지만 JDK1.8부터 에러아님
+			int liv4 = LV;
+		}
 	}
 }
