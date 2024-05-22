e7d446e8d20e670a2b3010571531916ef6d8a32d
pourquoi21 <miyaku219@naver.com>
2024-05-23 00:10:01 +0900

    static member

--- ch06/src/ch07/Ex7_15.java
+++ ch06/src/ch07/Ex7_15.java
@@ -5,6 +5,10 @@ public class Ex7_15 {
 		// 외부 클래스의 바깥에서 해당 외부 클래스의 내부 클래스 사용하는 방법
 		Outer2 oc = new Outer2(); // 외부 클래스의 인스턴스를 먼저 생성해야 인스턴스 클래스의
 		Outer2.InstanceInner ii = oc.new InstanceInner(); // 인스턴스를 생성 가능
+
+		System.out.println("ii.iv : " + ii.iv);
+		// static은 바로 쓸 수 있음
+		System.out.println("Outer2.StaticInner.cv : " + Outer2.StaticInner.cv);
 	}
 }
 
@@ -13,6 +17,11 @@ class Outer2 {
 	class InstanceInner {
 		int iv = 100;
 
-		// static은 바로 쓸 수 있음
 	}
+
+	static class StaticInner {
+		int iv = 200;
+		static int cv = 300;
+	}
+
 }
