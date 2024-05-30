8bf77bb215cd7c4f053ced6a8830f92f7b0d5ddd
pourquoi21 <miyaku219@naver.com>
2024-05-30 23:59:10 +0900

    queue

--- ch06/src/ch07/DiskQuiz.java
+++ ch06/src/ch07/DiskQuiz.java
@@ -1,5 +1,7 @@
 package ch07;
 
+import java.util.PriorityQueue;
+
 public class DiskQuiz {
 
 }
@@ -8,6 +10,13 @@ class Solution {
 	public int solution(int[][] jobs) {
 		int answer = 0;
 		int time = 0;
+
+		PriorityQueue<int[]> inQ = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
+		PriorityQueue<int[]> workQ = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+
+		for (int[] j : jobs) {
+			inQ.offer(j);
+		}
 
 		return answer;
 	}
