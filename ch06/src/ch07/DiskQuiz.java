3c189f55ed493730695131ab56e50ceac31b1878
pourquoi21 <miyaku219@naver.com>
2024-05-31 23:27:05 +0900

    anonymous

--- ch06/src/ch07/DiskQuiz.java
+++ ch06/src/ch07/DiskQuiz.java
@@ -4,9 +4,14 @@ import java.util.PriorityQueue;
 
 public class DiskQuiz {
 
+	public static void main(String[] args) {
+		Solution1 solution1 = new Solution1();
+		int[][] jobs = { { 0, 3 }, { 1, 9 }, { 2, 6 } };
+		solution1.solution(jobs);
+	}
 }
 
-class Solution {
+class Solution1 {
 	public int solution(int[][] jobs) {
 		int answer = 0;
 		int time = 0;
@@ -17,6 +22,24 @@ class Solution {
 		for (int[] j : jobs) {
 			inQ.offer(j);
 		}
+
+		while (!inQ.isEmpty() || !workQ.isEmpty()) {
+			while (!inQ.isEmpty() && inQ.peek()[0] <= time) {
+				workQ.offer(inQ.poll());
+			}
+
+			if (workQ.isEmpty()) {
+				time = inQ.peek()[0];
+			} else {
+				int[] j = workQ.poll();
+				answer += time + j[1] - j[0];
+				time += j[1];
+			}
+		}
+
+		answer /= jobs.length;
+
+		System.out.println(answer);
 
 		return answer;
 	}
--- ch06/src/ch07/Ex7_17.java
+++ ch06/src/ch07/Ex7_17.java
@@ -1,8 +1,8 @@
 package ch07;
 
 public class Ex7_17 {
-	Object iv = new Object() {
-		void method() {
+	Object iv = new Object() { // 조상이름을 대신쓴다. 조상또는 인터페이스
+		void method() { // 해당 클래스에서 쓰고싶은 함수를 넣는다.
 		}
 	}; // 익명클래스
 	static Object cv = new Object() {
