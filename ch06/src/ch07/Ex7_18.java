c7a97eccab95eb36b8972b3138c47b871af9dbc6
pourquoi21 <miyaku219@naver.com>
2024-06-02 23:55:56 +0900

    anonymous

--- ch06/src/ch07/Ex7_18.java
+++ ch06/src/ch07/Ex7_18.java
@@ -7,12 +7,17 @@ import java.awt.event.ActionListener;
 public class Ex7_18 {
 	public static void main(String[] args) {
 		Button b = new Button("Start");
-		b.addActionListener(new EventHandler());
+//		b.addActionListener(new EventHandler()); // 이걸 익명클래스로 바꾸려면
+		b.addActionListener(new ActionListener() { // 부모이름으로 클래스만들고
+			public void actionoPerformed(ActionEvent e) { // 내용 그대로 가져옴
+				System.out.println("ActionEvent occurred!!");
+			}
+		});
 	}
 }
 
-class EventHandler implements ActionListener {
-	public void actionoPerformed(ActionEvent e) {
-		System.out.println("ActionEvent occurred!!");
-	}
-}
+//class EventHandler implements ActionListener {
+//	public void actionoPerformed(ActionEvent e) {
+//		System.out.println("ActionEvent occurred!!");
+//	}
+//}
