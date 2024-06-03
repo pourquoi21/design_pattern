a61bf3af4c9bafb9294801b1df4b42f2d01c3eb1
pourquoi21 <miyaku219@naver.com>
2024-06-03 23:55:41 +0900

    anonymous end

--- ch06/src/ch07/Ex7_18.java
+++ ch06/src/ch07/Ex7_18.java
@@ -8,7 +8,7 @@ public class Ex7_18 {
 	public static void main(String[] args) {
 		Button b = new Button("Start");
 //		b.addActionListener(new EventHandler()); // 이걸 익명클래스로 바꾸려면
-		b.addActionListener(new ActionListener() { // 부모이름으로 클래스만들고
+		b.addActionListener(new ActionListener() { // 조상이름으로 클래스만들고(클래스의 정의와 객체 생성을 동시에)
 			public void actionPerformed(ActionEvent e) { // 내용 그대로 가져옴
 				System.out.println("ActionEvent occurred!!");
 			}
