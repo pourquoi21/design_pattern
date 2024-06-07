7ef16a3840c3ceed4d700584316cc3369d94b265
pourquoi21 <miyaku219@naver.com>
2024-06-07 23:54:58 +0900

    runtime error

--- ch06/src/ch08/ExceptionTest.java
+++ ch06/src/ch08/ExceptionTest.java
@@ -4,7 +4,8 @@ public class ExceptionTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		system.out.println("HELLO");
+//		system.out.println("HELLO");
+		System.out.println(args[0]); // 런타임 에러
 	}
 
 }
