adad78c8910d22f8cc70009f98fb911b1278d069
pourquoi21 <miyaku219@naver.com>
2024-06-08 23:46:12 +0900

    errors

--- ch06/src/ch08/ExceptionTest.java
+++ ch06/src/ch08/ExceptionTest.java
@@ -4,7 +4,7 @@ public class ExceptionTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		system.out.println("HELLO");
+//		system.out.println("HELLO"); // 컴파일 에러
 		System.out.println(args[0]); // 런타임 에러
 	}
 
