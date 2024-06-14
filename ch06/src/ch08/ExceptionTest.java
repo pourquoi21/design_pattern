fd32f1827f4fcaf8594db50fdec8faf8dc82367a
pourquoi21 <miyaku219@naver.com>
2024-06-14 23:56:25 +0900

    make exception on purpose

--- ch06/src/ch08/ExceptionTest.java
+++ ch06/src/ch08/ExceptionTest.java
@@ -6,6 +6,15 @@ public class ExceptionTest {
 		// TODO Auto-generated method stub
 //		system.out.println("HELLO"); // 컴파일 에러
 		System.out.println(args[0]); // 런타임 에러
+
+		try {
+			Exception e = new Exception("고의발생");
+			throw e;
+		} catch (Exception e) {
+			System.out.println("에러메시지 : " + e.getMessage());
+			e.printStackTrace();
+		}
+
 	}
 
 }
