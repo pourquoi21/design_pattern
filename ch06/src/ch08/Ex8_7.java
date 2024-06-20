dad54a8d23438baaf3c3cf0cd2620baba949dcfb
pourquoi21 <miyaku219@naver.com>
2024-06-20 23:33:30 +0900

    try - catch

--- ch06/src/ch08/Ex8_7.java
+++ ch06/src/ch08/Ex8_7.java
@@ -3,7 +3,11 @@ package ch08;
 public class Ex8_7 {
 
 	public static void main(String[] args) {
-		throw new RuntimeException();
+		try {
+			throw new RuntimeException();
+		} catch {
+			
+		}
 	}
 
 }
