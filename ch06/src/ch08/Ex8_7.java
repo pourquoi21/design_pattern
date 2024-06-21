c6dc98beabe7e5c79d589e4d2ea0125db5b875b9
pourquoi21 <miyaku219@naver.com>
2024-06-21 23:55:30 +0900

    checked and unchecked

--- ch06/src/ch08/Ex8_7.java
+++ ch06/src/ch08/Ex8_7.java
@@ -4,9 +4,9 @@ public class Ex8_7 {
 
 	public static void main(String[] args) {
 		try {
+			throw new Exception();
+		} catch (Exception e) {
 			throw new RuntimeException();
-		} catch {
-			
 		}
 	}
 
