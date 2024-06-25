b0871366e22f8da77d927da8c9a04de9ebc47670
pourquoi21 <miyaku219@naver.com>
2024-06-25 23:55:54 +0900

    pass

--- ch06/src/ch08/ExceptionExample.java
+++ ch06/src/ch08/ExceptionExample.java
@@ -1,7 +1,8 @@
 package ch08;
 
 public class ExceptionExample {
-	static void startInstall() throws SpaceException, MemoryException {
+	static void startInstall() throws SpaceException, MemoryException // 스스로 처리할 수 없는 예외는 떠넘긴다.
+	{
 		if (!enoughSpace()) {
 			throw new SpaceException("설치할 공간이 부족합니다.");
 		}
