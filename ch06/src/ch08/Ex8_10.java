06e6f9845a86eb4bc092cf8b13b22839eb7421c7
pourquoi21 <miyaku219@naver.com>
2024-06-28 23:28:29 +0900

    exception example

--- ch06/src/ch08/Ex8_10.java
+++ ch06/src/ch08/Ex8_10.java
@@ -1,10 +1,22 @@
 package ch08;
 
+import java.io.*;
+
 public class Ex8_10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			File f = createFile(args[0]);
+		}
+	}
 
+	static File createFile(String fileName) throws Exception {
+		if (fileName == null || fileName.equals("")) {
+			throw new Exception("유효하지 않은 파일명입니다.");
+		}
+		File f = new File(fileName);
+		f.createNewFile();
+		return f;
 	}
 
 }
