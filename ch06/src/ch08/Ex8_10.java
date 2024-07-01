951b3c6512432674acb1469d2a8e5d40dfa73ce1
pourquoi21 <miyaku219@naver.com>
2024-07-01 23:54:04 +0900

    myException

--- ch06/src/ch08/Ex8_10.java
+++ ch06/src/ch08/Ex8_10.java
@@ -1,25 +1,34 @@
 package ch08;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Ex8_10 {
 
 	public static void main(String[] args) {
-		try {
-			File f = createFile(args[0]);
-			System.out.println(f.getName() + "파일 성공적으로 생성됨");
-		} catch (Exception e) {
-			System.out.println(e.getMessage() + " 다시 입력해 주시기 바랍니다.");
-		}
+		File f = createFile("");
+		System.out.println(f.getName() + "파일 성공적으로 생성됨");
 	}
 	// args를 설정해주려면 run configuration에서 argument 적어준다.
 
-	static File createFile(String fileName) throws Exception {
-		if (fileName == null || fileName.equals("")) {
-			throw new Exception("유효하지 않은 파일명입니다.");
+	static File createFile(String fileName) {
+		try {
+			if (fileName == null || fileName.equals("")) {
+				throw new Exception("유효하지 않은 파일명입니다.");
+			}
+		} catch (Exception e) {
+			fileName = "noname.txt";
 		}
+
 		File f = new File(fileName);
-		f.createNewFile();
+		try {
+			f.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			System.out.println("finally block은 예외의 발생여부에 관계없이 항상 수행된다.");
+		}
 		return f;
 	}
 
---
+++ ch06/src/ch08/MyException.java
@@ -0,0 +1,7 @@
+package ch08;
+
+public class MyException extends Exception {
+	MyException(String msg) {
+		super(msg);
+	}
+}
