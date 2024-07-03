e4ec3dfbff344c4a06da3fe0b0db010b4df47bb1
pourquoi21 <miyaku219@naver.com>
2024-07-03 19:30:59 +0900

    constructor

--- ch06/src/ch08/MyException.java
+++ ch06/src/ch08/MyException.java
@@ -3,7 +3,7 @@ package ch08;
 public class MyException extends Exception {
 	private final int ERR_CODE;
 
-	MyException(String msg) {
+	MyException(String msg) { // String을 매개변수로 받는 생성자를 넣어준다.
 		super(msg);
 	}
 }
