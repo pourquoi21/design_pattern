853a473dee1e0a03af2d728d0975075e93692e31
pourquoi21 <miyaku219@naver.com>
2024-07-04 23:39:16 +0900

    re-throw

--- ch06/src/ch08/MyException.java
+++ ch06/src/ch08/MyException.java
@@ -6,4 +6,7 @@ public class MyException extends Exception {
 	MyException(String msg) { // String을 매개변수로 받는 생성자를 넣어준다.
 		super(msg);
 	}
+
+	// 예외 되던지기라는 개념도 있다.
+	// 메서드 내에서 약간 처리한 다음 main에서 나머지를 처리한다든지..
 }
