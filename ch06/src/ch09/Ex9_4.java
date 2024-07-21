0e342b33b2a246a3532be81caed45e0b41560167
pourquoi21 <miyaku219@naver.com>
2024-07-21 22:47:53 +0900

    original toString

--- ch06/src/ch09/Ex9_4.java
+++ ch06/src/ch09/Ex9_4.java
@@ -1,14 +1,20 @@
 package ch09;
 
 public class Ex9_4 {
+	public static void main(String[] args) {
+		Card c1 = new Card();
+		Card c2 = new Card();
 
+		System.out.println(c1.toString()); // 이건 별로 도움이 안된다.
+		System.out.println(c2.toString());
+	}
 }
 
 class Card {
 	String kind;
 	int number;
 
-	Card()_ {
+	Card() {
 		this("SPADE", 1);
 	}
 
